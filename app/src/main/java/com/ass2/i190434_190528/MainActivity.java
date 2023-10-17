8394b7dd814d1bc5b0cbada3eea258ca524f620e
Sourav Malani <i190434@nu.edu.pk>
2023-10-18 00:24:45 +0500

    Added Splash Screen

--- app/src/main/java/com/ass2/i190434_190528/MainActivity.java
+++ app/src/main/java/com/ass2/i190434_190528/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     public void onSignUpClicked(View view) {
         Intent intent = new Intent(this, registration.class);
---
+++ app/src/main/java/com/ass2/i190434_190528/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.ass2.i190434_190528;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000); // Display splash screen for 5 seconds
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+}
